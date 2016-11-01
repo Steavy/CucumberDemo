444e497037ec0433dd49a39a2117b975aff7d2fb
Steven Nienhuis <djsteavy@gmail.com>
2016-11-01 20:46:49 +0100

    SharedDriver zo gewijzigd dat deze ook met Chrome kan testen

--- src/test/java/com/atos/cucumberdemo/step/SharedDriver.java
+++ src/test/java/com/atos/cucumberdemo/step/SharedDriver.java
@@ -10,7 +10,9 @@ import cucumber.api.java.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
     /**
@@ -34,7 +36,8 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
      * </p>
      */
     public class SharedDriver extends EventFiringWebDriver {
-        private static final WebDriver REAL_DRIVER = new FirefoxDriver();
+        //System.setProperty("webdriver.chrome.driver", "C://driver//chromedriver.exe");
+        private static final WebDriver REAL_DRIVER;
         private static final Thread CLOSE_THREAD = new Thread() {
             @Override
             public void run() {
@@ -43,6 +46,27 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
         };
 
         static {
+            String browserName = System.getProperty("browser");
+            if(browserName == null) {
+                browserName = "chrome";
+            }
+            if(browserName.equals("firefox")) {
+                REAL_DRIVER = new FirefoxDriver();
+            }
+            else if(browserName.equals("chrome")) {
+                String pathToDriver = System.getProperty("pathToDriver");
+                if(pathToDriver == null) {
+                    pathToDriver = "C://driver//chromedriver.exe";
+                }
+                System.setProperty("webdriver.chrome.driver", pathToDriver);
+                REAL_DRIVER = new ChromeDriver();
+            }
+            else if(browserName.equals("internetExplorer")) {
+                REAL_DRIVER = new InternetExplorerDriver();
+            }
+            else {
+                throw new RuntimeException("You have not specified a valid browser");
+            }
             Runtime.getRuntime().addShutdownHook(CLOSE_THREAD);
         }
 
